a3e4f908e5e07bdb1e6241869b5dc9648ad9a679
RyanGD2025 <ryanbordez@gmail.com>
2025-12-05 19:38:52 -0300

    Create MainActivity.kt

---
+++ platform/android/app/src/main/java/com/cubemusic/studio/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.cubemusic.studio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+
+    companion object {
+        init {
+            System.loadLibrary("audiocore") 
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            
+        }
+        
+        val initialized = initializeAudioEngine(44100, 512)
+        if (initialized) {
+            startPlayback()
+        }
+    }
+    
+    private external fun initializeAudioEngine(sampleRate: Int, framesPerBuffer: Int): Boolean
+    private external fun startPlayback()
+    private external fun stopPlayback()
+    private external fun applyReverbToTrack(trackId: Int, dryWet: Float)
+}
